f0204b9cd39cfcda718cd10f4a2596268650d743
Jesse De Jong <jdoceau@gmail.com>
2022-04-17 13:10:52 +1000

    fixed css not being applied, added google fonts, imported some scss from prev, built initial landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,8 +1,22 @@
-
+import '../styles/reset.css';
+import '../styles/style.css';
 
 const Landing = () => {
+
+    // less 75 pixels to compensate for the navbar
+    const windowHeight = document.documentElement.clientHeight - 75;
+  
+    const styles = {
+        div: {
+          height: windowHeight
+        }
+    }
+
     return (
-        <h1>Landing Page</h1>
+        <section style={styles.div} class="landing">
+            <h1 class="landing__heading">Jesse DE Jong</h1>
+            <p class="landing__script">web <a href="https://github.com/Jesse-DeJong" target="_target" alt="github link"><i class="fa-brands fa-github"></i></a> dev</p>
+        </section>
     )
 }
 
